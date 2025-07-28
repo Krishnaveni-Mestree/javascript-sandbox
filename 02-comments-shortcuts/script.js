ded3094d1a44502c5ef875b7a42c9376922160e3
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-28 12:39:28 +0530

    defined comments & shorcuts in javascript

---
+++ 02-comments-shortcuts/script.js
@@ -0,0 +1,21 @@
+// This is a single line of code
+
+/*
+Multi-line comment
+console.log(100);
+
+console.log('Hello World');
+
+console.log(20, 'Hello', true);
+console.log(20, 'Hello', true);
+*/
+
+//---------------------------------------------------------
+//shortcuts
+//ctrl+/ = comment/uncomment (toggling)
+//shift+up/down = can highlight the code by giging down or up
+//shift+right/left = can highlight the code by giging left or right
+//ctrl+right/left = navigating to the words of code
+//alt+up/down = moving line of code
+//ctrl+shift+L = selecting all words at a time
+//ctrl+~ = toggling terminal
